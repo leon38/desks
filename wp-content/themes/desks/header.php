7c11dd45f8f0125b1a3408bbf937a9afb2ac04ed
Damien Corona <leoncorono@gmail.com>
2014-04-23 18:51:19 +0200

    initial commit

---
+++ wp-content/themes/desks/header.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The Header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<header id="header">
+	<div class="container">
+	<div id="logo">
+
+	</div>
+	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'container_id' => 'menu' ) ); ?>
+	<div id="profile">
+		<ul>
+			<li><a href="javascript:">Your Account</a></li>
+		</ul>
+	</div>
+	<div id="features"></div>
+	<div class="clearfix"></div>
+	</div>
+</header>
+<div class="container">
